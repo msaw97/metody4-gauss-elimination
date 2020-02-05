4e3d42c70410da434c872f884ceade85dfec7182
milosz <milosz@deb.home>
2020-02-05 22:02:34 +0000

    Added exception for the inaccurate LU decomp result.

--- main.py
+++ main.py
@@ -8,19 +8,24 @@ for equation in examples:
 
     print("Gauss regular solution:")
     try:
-        print(f"X: {Gauss_regular(equation.A, equation.B)}")
+        XR = Gauss_regular(equation.A, equation.B)
+        print("X: {}".format(XR))
     except ZeroDivisionError as err:
         print("Error:", err)
 
     print(f"Gauss scaled solution:")
     try:
-        print(f"X: {Gauss_scaled(equation.A, equation.B)}")
+        XS = Gauss_scaled(equation.A, equation.B)
+        print("X: {}".format(XS))
     except ZeroDivisionError as err:
         print("Error:", err)
 
     print("LU decomposition of matrix A using a regular Gauss method:")
     try:
         L,U = Doolittle(equation.A)
-        print("Matrix L: \n{} \nMatrix U: \n{}\n".format(L, U))
+        if not np.allclose(XS, XR):
+            print("Inaccurate result when using a regular Gauss method.\n")
+        else:
+            print("Matrix L: \n{} \nMatrix U: \n{}\n".format(L, U))
     except ZeroDivisionError as err:
         print("Error: {}\n".format(err))
